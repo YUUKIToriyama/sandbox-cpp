ccb58a792cb19bc35e54aefe9ce798ae7335fa7c
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-09-15 20:03:57 +0900

    演算子+-をオーバーロード

--- vector3.cpp
+++ vector3.cpp
@@ -7,21 +7,43 @@ struct Vector3 {
 	double y;
 	double z;
 
+	// 加算
+	auto operator + (Vector3 vec) {
+		Vector3 vec_new;
+		vec_new.x = x + vec.x;
+		vec_new.y = y + vec.y;
+		vec_new.z = z + vec.z;
+		return vec_new;
+	}
+	// 減算
+	auto operator - (Vector3 vec) {
+		Vector3 vec_new;
+		vec_new.x = x - vec.x;
+		vec_new.y = y - vec.y;
+		vec_new.z = z - vec.z;
+		return vec_new;
+	}
+
+	// ベクトルの長さ
 	double length() {
 		return sqrt(x*2 + y*2 + z*2);
 	}
+	// 内積(ドット積)
 	double dot(Vector3 vec) {
 		return x * vec.x + y * vec.y + z * vec.z;
 	}
-	
-	auto product(Vector3 vec) {
+	// 外積(クロス積)
+	auto cross(Vector3 vec) {
 		Vector3 vec_new;
 		vec_new.x = y * vec.z - z * vec.y;
 		vec_new.y = z * vec.x - x * vec.z;
 		vec_new.z = x * vec.y - y * vec.x;
 		return vec_new;
 	}
-	
+	// ベクトルを表示する
+	void show() {
+		cout << "(" << x << ", " << y << ", " << z << ")" << endl;
+	}
 };
 
 int main() {
@@ -30,6 +52,10 @@ int main() {
 
 	cout << a.length() << endl;
 	cout << a.dot(b) << endl;
+
+	a.cross(b).show();
+	(a + b).show();
+	(a - b).show();
 
 	return 0;
 }
