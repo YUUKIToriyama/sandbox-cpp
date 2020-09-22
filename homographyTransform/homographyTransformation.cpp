dcb283997c22ff799ee0291ea4f6b456cc90417f
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-09-22 20:16:48 +0900

    リファクタリングした

--- homographyTransform/homographyTransformation.cpp
+++ homographyTransform/homographyTransformation.cpp
@@ -1,71 +1,86 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat inputImage, inputImage_copy;
+vector<Point2f> vertices;
+Scalar myColor = {255, 255, 0, 0.7};
+int num;
+void eventCallback(int event, int x, int y, int flags, void* userdata);
+Mat doHomographyTransformation(int WIDTH, int HEIGHT);
 
 
-cv::Mat image;
-std::vector<cv::Point2f> points;
-int i = 0;
-int WIDTH = 500;
-int HEIGHT = 500;
-
-cv::Mat homographyTransformation(std::vector<cv::Point2f> v, cv::Mat img) {
-	std::vector<cv::Point2f> projectionPoint2fs = {
-		cv::Point2f(0,0),
-		cv::Point2f(WIDTH, 0),
-		cv::Point2f(WIDTH, HEIGHT),
-		cv::Point2f(0,  HEIGHT)
-	};
-
-	cv::Mat homographyMatrix = cv::getPerspectiveTransform(v, projectionPoint2fs);
-	cv::Mat outputImage = cv::Mat::zeros(WIDTH, HEIGHT, 8);
-	cv::warpPerspective(img, outputImage, homographyMatrix, outputImage.size());
-
-	return outputImage;
-}
-
-
-void eventCallback(int event, int x, int y, int flags, void* userdata) {
-	if (event == cv::EVENT_LBUTTONDOWN) {
-		std::cout << "position: " << x << "," << y << std::endl;
-		cv::circle(image, cv::Point2f(x,y), 10, cv::Scalar(255,255,255), 2, cv::LINE_8);
-		points.push_back(cv::Point2f(x,y));
-		if (4 > i && i > 0) {
-			cv::line(image, points[i-1], points[i], cv::Scalar(0,255,255), 2);
-			if (i == 3) {
-				cv::line(image, points[3], points[0], cv::Scalar(255,255,0), 2);
-				cv::Mat output = homographyTransformation(points, image);
-				cv::imshow("result", output);
-				cv::waitKey(0);
-				cv::imwrite("aaa.png", output);
-			}
-		}
-		i++;
-		
-		if (points.size() == 4) {
-			std::cout << points << std::endl;
-		}
-	}
-}
-
-int main(int argc, char **argv) {
-	if (argc < 2) {
-		std::cout << "Usage: ./command [input_file]" << std::endl;
+int main(int argc, char** argv) {
+	// 引数が与えられていない場合終了する
+	if (argc <= 1) {
+		cout << "Usage: ./homographyTransform [input_file]" << endl;
 		return -1;
-	} else {
-		std::string inputFileName = argv[1];
-		image = cv::imread(inputFileName, cv::IMREAD_COLOR);
 	}
-
-	cv::namedWindow("myWindow", cv::WINDOW_AUTOSIZE);
+	// 引数からファイル名を読み取り変数inputImageに読み出す
+	// inputImage_copyはそのコピー。後に用いる
+	string fileName = argv[1];
+	inputImage = imread(fileName, IMREAD_COLOR);
+	inputImage_copy = inputImage.clone();
+	
+	namedWindow("main", WINDOW_NORMAL);
+	int keyCode;
 	while (true) {
-		cv::imshow("myWindow", image);
-		cv::setMouseCallback("myWindow", eventCallback, NULL);
-		int key = cv::waitKey(1);
-		if (key != -1) {
+		imshow("main", inputImage);
+		setMouseCallback("main", eventCallback, NULL);
+		keyCode = waitKey(1);
+		if (keyCode != -1) {
 			break;
 		}
 	}
 	return 0;
+}
+
+void eventCallback(int event, int x, int y, int flags, void* userdata) {
+	// 左ボタンをクリックすると頂点を選択できる
+	if (event == EVENT_LBUTTONDOWN) {
+		if (4 > num) {
+			// クリックした位置にマーカーを描画
+			circle(inputImage, Point2f(x,y), 10, myColor, 2, LINE_8);
+			// 四角形の頂点の座標をベクタに格納
+			vertices.push_back(Point2f(x,y));
+			// 2点目からはその点とその前の点とを結ぶ線分を描く
+			if (num != 0) {
+				line(inputImage, vertices[num-1], vertices[num], myColor, 2);
+			}
+			// 4点指定したら最初の点と4点目を結び四角形にする
+			if (num == 3) {
+				line(inputImage, vertices[3], vertices[0], myColor, 2);
+				//fillPoly(inputImage, vertices, myColor, 8, 0);
+				// ホモグラフィ変換
+				Mat result = doHomographyTransformation(500, 500);
+				imshow("result", result);
+				imwrite("result.png", result);
+				waitKey(0);
+			}
+			num = num + 1;
+		}
+	}
+	// 右ボタンをクリックするとリセットできる
+	if (event == EVENT_RBUTTONDOWN) {
+		inputImage = inputImage_copy.clone();
+		num = 0;
+		vertices.clear();
+	}
+}
+
+Mat doHomographyTransformation(int WIDTH, int HEIGHT) {
+	vector<Point2f> v = {
+		Point2f(0, 0),
+		Point2f(WIDTH, 0),
+		Point2f(WIDTH, HEIGHT),
+		Point2f(0, HEIGHT)
+	};
+	Mat homographyMatrix = getPerspectiveTransform(vertices, v);
+	Mat output = Mat::zeros(WIDTH, HEIGHT, 8);
+	warpPerspective(inputImage_copy, output, homographyMatrix, output.size());
+	return output;
 }
